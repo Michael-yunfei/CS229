9576d9cc8b121922c7afe6ebb8797e332298294b
Michael-cuhk <michael.yunfei@gmail.com>
2020-06-13 23:23:08 +0100

    finish the code for case study

--- Code/Quasar_Spectra.py
+++ Code/Quasar_Spectra.py
@@ -87,7 +87,7 @@ ax.set(xlabel='Wavelength', ylabel='Flux')
 
 # tain the regression
 pred = []
-for k, x_j in enumerate(x):
+for k, x_j in enumerate(x):  # automatically enumerate each row
     w = build_weights(x, x_j, 5)
     theta = normal_equation(x, y, w)
     pred.append((theta.T @ x_j[:, np.newaxis]).ravel()[0])
@@ -121,38 +121,71 @@ for k, tau in enumerate(np.logspace(0, 3, 4).astype(int)):
     ax.set(xlabel="wavebadth", ylabel='Flux', title='tau = {}'.format(tau))
 
 
+# Smooth the data
+y_train = quasar_train.values.T
+y_train.shape
+y_test = quasar_test.values.T
 
 
+def smoother(x, y_in, tau):
+    '''
+    A function that repeats prediction for all observations
+    Input
+    ------
+    x : m by 2, matrix, one column is constant, one column is wavebadth
+    y_in : obsevations we got, or noise
+    tau : the weight parameter
+    Output
+    ------
+    Pred : a m by n matrix, which includes all the predications
+    '''
+    pred = np.zeros(y_in.shape)
+    for i in range(y_in.shape[1]):
+        y = y_in[:, i]  # get each column, or each observation
+        for j, x_j in enumerate(x):
+            w = build_weights(x, x_j, tau)  # construct weights
+            theta = normal_equation(x, y, w)
+            pred[j, i] = (theta.T @ x_j[:, np.newaxis]).ravel()[0]
+    return pred
 
 
+y_train_smooth = smoother(x, y_train, 5)
+y_train_smooth.shape  # (450, 200)
+y_test_smooth = smoother(x, y_test, 5)
+y_test_smooth.shape  # (450, 50)
 
 
+# understand np.argsort
+np.argsort(np.array([1, 3, 4, 2]))
+# the postion for 1 is zero,
+# the position for 3 is 3
+# the position for 4 is 1
+# sorted_array(1, 2, 3, 4) = position_array([0, 3, 1, 2])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# I didnot finsih all questions for this case study
+#
+# k = 3
+# estimation = []
+# errors = []
+# train_left, train_right = np.split(y_train_smooth,[150],axis=1)
+# for i in range(train_right.shape[0]):
+#     row = train_right[i]
+#     dist = ((train_right-row)**2).sum(axis=1)
+#     h = dist.max()
+#
+#     neighbors = dist.argsort()[:k]
+#
+#     bottom = 0
+#     for j in range(len(neighbors)):
+#         bottom += ker(dist[neighbors[j]]/h)
+#     left_hat = np.zeros(len(train_left[0]))
+#     for j in range(len(neighbors)):
+#         left_hat += ker(dist[neighbors[j]]/h)*train_left[neighbors[j]]
+#     left_hat /= bottom
+#     estimation.append(left_hat)
+#     error = np.sum((train_left[i] - left_hat) ** 2)
+#     errors.append(error)
+# errors = np.array(errors)
+# estimation = np.array(estimation)
+# print('The average error is %.6f'%errors.mean())
 #
